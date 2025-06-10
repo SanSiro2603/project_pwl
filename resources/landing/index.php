0b917a75b7d22654f1f7b616966b8ae81565d9fe
KamuCantik <68129435+wenkzofficial@users.noreply.github.com>
2025-06-11 00:05:35 +0700

    asd
    asd

---
+++ resources/landing/index.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Responsive Landing Page | ScrollReveal &amp; Tiny Slider</title>
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.4/tiny-slider.css'>
+    <link rel="stylesheet" href="./style.css">
+
+</head>
+
+<body>
+    <!-- partial:index.partial.html -->
+
+    <body>
+        <section class="home">
+            <div class="description">
+                <h1 class="title">
+                    <span class="gradient-text">Studio Foto</span>
+                </h1>
+                <p class="paragraph">
+                    Selamat datang di studio foto kami — tempat di mana setiap momen berharga diubah menjadi karya seni abadi. Dengan peralatan profesional, pencahayaan yang presisi, dan fotografer berpengalaman, kami siap menangkap setiap detail dan ekspresi yang penuh
+                    makna. Baik untuk sesi prewedding, keluarga, produk, hingga kebutuhan personal branding, kami hadir untuk memberikan hasil terbaik dengan pengalaman yang nyaman dan menyenangkan. Jadwalkan sesi fotomu hari ini dan rasakan perbedaannya!
+                </p>
+                <button type="submit" class="btn" aria-label="submit">
+  <span>Login</span>
+  <ion-icon name="arrow-forward-outline"></ion-icon>
+</button>
+
+                <button type="submit" class="btn" aria-label="submit" style="margin-top: 10px;">
+  <span>Register</span>
+  <ion-icon name="arrow-forward-outline"></ion-icon>
+</button>
+
+            </div>
+
+            <div class="users-color-container">
+                <span class="item" style="--i: 1"></span>
+                <img class="item" src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/274f29ce-0d3f-4ac2-a2aa-f9b7bd188b2a" style="--i: 2" alt="" />
+                <span class="item" style="--i: 3"></span>
+                <img class="item" src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/b8a14493-3d9f-4b9b-b93a-56d0bc7243e9" style="--i: 4" alt="" />
+
+                <img class="item" src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/03e51e1e-9750-45a5-b75e-a1e341d4562a" style="--i: 10" alt="" />
+                <span class="item" style="--i: 11"></span>
+                <img class="item" src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/5eb50f89-3e5a-480e-860c-8d40d3ba9ffe" style="--i: 12" alt="" />
+                <span class="item" style="--i: 5"></span>
+
+                <span class="item" style="--i: 9"></span>
+                <img class="item" src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/86c71a79-2efe-4567-8665-b1e5a1fd9735" style="--i: 8" alt="" />
+                <span class="item" style="--i: 7"></span>
+                <img class="item" src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/97ef9643-5202-41aa-80f0-ceeabccdd099" style="--i: 6" alt="" />
+            </div>
+        </section>
+
+        <section class="card-container" id="card-container">
+            <div class="slider">
+                <div class="card" data-tilt>
+                    <div class="content">
+                        <img src="https://demosphoto.com/wp-content/uploads/2022/06/arrangement-professional-photographer-equipment-scaled.jpg" alt="" />
+                        <h1>Studio Indoor Profesional</h1>
+                        <p>
+                            Kami menyediakan studio indoor dengan pencahayaan dan peralatan profesional, cocok untuk berbagai kebutuhan seperti foto keluarga, wisuda, prewedding, dan personal branding.
+                        </p>
+                        <button class="btn btn-grad">
+              <span>Lebih Lanjut</span>
+              <ion-icon name="arrow-forward-outline"></ion-icon>
+            </button>
+                    </div>
+                </div>
+
+                <div class="card" data-tilt>
+                    <div class="content">
+                        <img src="https://media-cdn.tripadvisor.com/media/photo-s/0d/27/c3/c6/enjoy-cosplay.jpg" alt="" />
+                        <h1>Konsep & Tema Custom</h1>
+                        <p>
+                            Buat sesi foto impianmu jadi nyata! Kami menerima permintaan konsep khusus seperti tema tradisional, modern, casual, formal, bahkan cosplay dan cinematic.
+                        </p>
+                        <button class="btn btn-grad">
+              <span>Lebih Lanjut</span>
+              <ion-icon name="arrow-forward-outline"></ion-icon>
+            </button>
+                    </div>
+                </div>
+
+                <div class="card" data-tilt>
+                    <div class="content">
+                        <img src="https://www.adobe.com/creativecloud/photography/type/media_171fe4140fd887c18abbb53771eccf138c090399b.png?width=2000&format=webply&optimize=medium" alt="" />
+                        <h1>Foto Produk & UMKM Friendly</h1>
+                        <p>
+                            Tingkatkan daya tarik produkmu dengan foto berkualitas tinggi. Cocok untuk katalog, marketplace, atau sosial media brand UMKM maupun perusahaan.
+                        </p>
+                        <button class="btn btn-grad">
+              <span>Lebih Lanjut</span>
+              <ion-icon name="arrow-forward-outline"></ion-icon>
+            </button>
+                    </div>
+                </div>
+
+                <div class="card" data-tilt>
+                    <div class="content">
+                        <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj-pqHd9byfviZpWVZAkcd6z1qPVsyzXo39dIhVKHKCVaXoIkGwUjPigzWNxUIHyCHDAJTadqgCEupvARbS4fsE-nDpO0IesHyLP9YfFn8aKLNyWU3ai50AL00h603eRi_ZlmIL3PKcGzY/s1600/FCP2.jpg" alt="" />
+                        <h1>Retouch & Editing Profesional</h1>
+                        <p>
+                            Kami tidak hanya memotret, tapi juga mengedit dengan teliti agar hasil fotomu tampak maksimal dan siap dipakai untuk berbagai keperluan.
+                        </p>
+                        <button class="btn btn-grad">
+              <span>Lebih Lanjut</span>
+              <ion-icon name="arrow-forward-outline"></ion-icon>
+            </button>
+                    </div>
+                </div>
+
+                <div class="card" data-tilt>
+                    <div class="content">
+                        <img src="https://youngontop.com/wp-content/uploads/2024/10/Picture.png" alt="" />
+                        <h1>Cetak & Merchandise Foto</h1>
+                        <p>
+                            Kami menyediakan cetak foto, album eksklusif, photobook, frame, bahkan souvenir seperti mug dan kalender. Semua bisa custom sesuai keinginanmu.
+                        </p>
+                        <button class="btn btn-grad">
+              <span>Lebih Lanjut</span>
+              <ion-icon name="arrow-forward-outline"></ion-icon>
+            </button>
+                    </div>
+                </div>
+            </div>
+
+            <ul class="control" id="custom-control">
+                <li>
+                    <ion-icon name="caret-back-outline"></ion-icon>
+                </li>
+                <li>
+                    <ion-icon name="caret-forward-outline"></ion-icon>
+                </li>
+            </ul>
+        </section>
+    </body>
+    <!-- partial -->
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.2/min/tiny-slider.js'></script>
+    <script src='https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js'></script>
+    <script src='https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js'></script>
+    <script src='https://unpkg.com/scrollreveal'></script>
+    <script src="./script.js"></script>
+
+</body>
+
+</html>
